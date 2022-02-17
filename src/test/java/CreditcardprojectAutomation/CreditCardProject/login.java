9c1154bac85d81bef619406b379b23e66bebaf20
gokul <gokul@cgs.com>
2022-02-17 12:08:08 +0530

    The URl check done by gokul on 17/2/2022

--- src/test/java/CreditcardprojectAutomation/CreditCardProject/login.java
+++ src/test/java/CreditcardprojectAutomation/CreditCardProject/login.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
 
 public class login 
 {
@@ -37,11 +38,17 @@ WebDriver driver;
 	    Submit.click();
 	    System.out.println("Login function completed"); // This is committed by gokul
     }
-//    @Test(priority =1)
-//    public void afterlogin()
-//    {
-//    	
-//    }
+   @Test(priority =1)
+  public void afterlogincheckwhethercorrecturl()
+  {
+	String Expected="http://172.16.10.74:8186/StarCreditApp/#/login";   
+    String CurrentUrl = driver.getCurrentUrl();
+    Assert.assertEquals(Expected, CurrentUrl);
+    System.out.println("pass");
+    {
+    	System.out.println("fail"); // URL check done gokul on 17/02/2022
+    }
+  }
     @AfterMethod
     public void driverclose()
     {
